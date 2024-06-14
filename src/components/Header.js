06c5dbe74d20a5f5b2c670432dd51312f94e413c
Aulia Permana <permanaaulia@gmail.com>
2024-06-14 13:44:55 +0700

    repair new

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,16 +58,17 @@ export default function Header() {
 
         {/* Hamburger Menu Button */}
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="flex items-center focus:outline-none">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              {isMenuOpen ? (
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
-              ) : (
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-              )}
-            </svg>
-          </button>
-        </div>
+  <button onClick={toggleMenu} className="flex items-center focus:outline-none" aria-label="Toggle Menu">
+    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      {isMenuOpen ? (
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+      ) : (
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+      )}
+    </svg>
+  </button>
+</div>
+
       </div>
     </header>
   );
